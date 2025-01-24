a1bc18b033bc705b9ffa1d20e6edba1322ca573b
Chrysostomos Koumides <chyrkoum@gmail.com>
2025-01-24 20:41:40 +0200

    chore: remove unused Lottie integration and clean up gradient background logic

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
 import { FaLocationArrow } from "react-icons/fa6";
 
-// Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
-
 import { cn } from "@/lib/utils";
 
 import { BackgroundGradientAnimation } from "./GradientBg";
@@ -180,14 +177,6 @@ export const BentoGridItem = ({
               {/* add rounded-md h-8 md:h-8, remove rounded-full */}
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
-              <div
-                className={`absolute -bottom-5 right-0 ${
-                  clicked ? "block" : "block"
-                }`}
-              >
-                {/* <img src="/confetti.gif" alt="confetti" /> */}
-                <Lottie options={defaultOptions} height={200} width={400} />
-              </div>
               <MagicButton
                 title="Call for Participants"
                 icon={<FaLocationArrow />}
